d80ee1106ab3da3a75adf571b0a55dde703cedc1
Zyg0m4t1k <l4ss4t3r@gmail.com>
2019-08-22 20:12:44 +0200

    ...

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -21,10 +21,9 @@ require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 
 
 function speedtest_update() {
-
-		log::add('speedtest', 'error', '!!! Voir le changelog et doc pour les changements!!!');
-
-    
+	 foreach (speedtest::byType('speedtest') as $speedtest) {
+		 $speedtest->save();		 
+	 }	
 }
 
 
